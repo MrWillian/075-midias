3b1f4871f8b6dee32de74ffe1de60fdcfede8cd0
MrWillian <willian.dev10@gmail.com>
2022-07-29 11:59:06 -0300

    refactor: add Navbar component

---
+++ src/components/Navbar/style.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import { NavLink as Link } from 'react-router-dom';
+
+export const Nav = styled.nav`
+    background #000;
+    height: 60px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 10rem;
+`;
+
+export const NavLinkContainer = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 2rem;
+    z-index: 10;
+`;
+
+export const Logo = styled.button`
+    border: none;
+    cursor: pointer;
+    align-items: center;
+`;
+
+export const NavLink = styled(Link)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    font-size: 0.8rem;
+
+    &.active {
+        color: #C75104;
+    }
+`;
+
+export const Button = styled(Link)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    width: 80px;
+    border: 1px solid #C75104;
+    border-radius: 1rem;
+    cursor: pointer;
+    background-color: #C75104;
+
+    font-size: 0.9rem;
+    color: #FFF;
+    text-decoration: none;
+`;
