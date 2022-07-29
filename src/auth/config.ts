de1532809612bc1ecf2d458a9b2beeac3f26c9cf
MrWillian <willian.dev10@gmail.com>
2022-07-29 11:59:50 -0300

    config: add auth config

---
+++ src/auth/config.ts
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+export const authConfig = firebase.initializeApp({
+    apiKey: "AIzaSyDxDOL4RKCH_4XPRnilLMG6yYoWmATvS60",
+    authDomain: "midias-075.firebaseapp.com",
+    projectId: "midias-075",
+    storageBucket: "midias-075.appspot.com",
+    messagingSenderId: "521127631150",
+    appId: "1:521127631150:web:d922a66b3001a85c8882bf",
+    measurementId: "G-ER37PWWEZV"
+});
