8d940b55321a9e6aaf3ddf090f2da6921cb02e4d
MrWillian <willian.dev10@gmail.com>
2022-08-01 21:43:15 -0300

    fix: set alt to image

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
             <C.Nav>
                 <C.Logo>
-                    <img src="../assets/logo.png" height="50" width="100" />
+                    <img src="../assets/logo.png" alt="logo" height="50" width="100" />
                 </C.Logo>
                 <C.NavLinkContainer>
                     <C.NavLink to="/">
