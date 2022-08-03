9ebafa2691bd1e75e937b80de8d0621b5c83e92a
MrWillian <willian.dev10@gmail.com>
2022-08-03 09:00:46 -0300

    chore: comment unused import

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import ContactSection from '../../components/ContactSection';
 import EventsSection from '../../components/EventsSection';
 import Navbar from '../../components/Navbar';
