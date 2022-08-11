07b38fb7cc0934a4a61b278b8af589d01152a9c9
MrWillian <willian.dev10@gmail.com>
2022-08-10 22:38:38 -0300

    chore: add success response enum and signin return

--- src/@types/auth.d.ts
+++ src/@types/auth.d.ts
@@ -10,6 +10,10 @@ export interface IUser {
     password: string;
 }
 
+export enum SuccessSigninResponse {
+    Success =  "Usuário Logado com Sucesso!"
+}
+
 export enum ErrorSigninResponse {
     WrongEmailOrPassword =  "E-mail ou senha incorretos",
     UserNotFound = "Usuário não cadastrado",
@@ -18,7 +22,7 @@ export enum ErrorSigninResponse {
 export type AuthContextType = {
     user?: IUser | null;
     signed: boolean;
-    signin: (email: string, password: string) => string | undefined | void;
+    signin: (email: string, password: string) => Promise<SuccessSigninResponse | ErrorSigninResponse | undefined>;
     signup: (email: string, password: string) => string | undefined | void;
     signout: (id: number) => void;
 };
