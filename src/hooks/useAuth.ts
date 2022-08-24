41eaf9bf21a0477a847d0280dc8be1609fe7deb9
MrWillian <willian.dev10@gmail.com>
2022-08-23 21:21:13 -0300

    fix: set AuthContextType

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,8 +1,9 @@
 import { useContext } from "react";
+import { AuthContextType } from "../@types/auth";
 import { AuthContext } from "../contexts/auth";
 
 const useAuth = () => {
-    return useContext(AuthContext);
+    return useContext(AuthContext) as AuthContextType;
 }
 
 export default useAuth;
