57996dc9188ad6cf015435aa7e5bd3d2294361a5
MrWillian <willian.dev10@gmail.com>
2022-08-23 21:21:55 -0300

    refactor: change navbar according user logged or not

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import * as C from './style';
 
 const Navbar = () => {
+    const { user, signout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSignout = () => {
+        signout();
+        navigate(0);
+    }
+
     return (
         <>
             <C.Nav>
@@ -9,18 +19,32 @@ const Navbar = () => {
                     <img src="../assets/logo.png" alt="logo" height="50" width="100" />
                 </C.Logo>
                 <C.NavLinkContainer>
-                    <C.NavLink to="/">
-                        <h1>Início</h1>
-                    </C.NavLink>
-                    <C.NavLink to="/">
-                        <h1>Evento</h1>
-                    </C.NavLink>
-                    <C.NavLink to="/">
-                        <h1>Contato</h1>
-                    </C.NavLink>
-                    <div>
-                        <C.Button to="/signin">Entrar</C.Button>
-                    </div>
+                    <>
+                        <C.NavLink to="/">
+                            <h1>Início</h1>
+                        </C.NavLink>
+                        <C.NavLink to="/">
+                            <h1>Evento</h1>
+                        </C.NavLink>
+                        <C.NavLink to="/">
+                            <h1>Contato</h1>
+                        </C.NavLink>
+                        {user !== undefined ?
+                            <C.NavLink to="/">
+                                <h1>Albuns</h1>
+                            </C.NavLink>
+                            : <></>
+                        }
+                        {user === undefined ?
+                            (<div>
+                                <C.Button to="/signin">Entrar</C.Button>
+                            </div>)
+                        :
+                            (<div>
+                                <C.btnSignout onClick={handleSignout}>Sair</C.btnSignout>
+                            </div>)
+                        }
+                    </>
                 </C.NavLinkContainer>
             </C.Nav>
         </>
