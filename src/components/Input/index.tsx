6e4bda71908bc5381b84b6cb8a7a4937dbe232a9
MrWillian <willian.dev10@gmail.com>
2022-09-23 11:36:07 -0300

    chore: increase name prop

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,15 +3,17 @@ import * as C from "./styles";
 
 type Props = {
     type?: React.HTMLInputTypeAttribute,
+    name?: string,
     placeholder?: string,
     value?: string | number | readonly string[],
     onChange?: React.ChangeEventHandler<HTMLInputElement>,
 }
 
-const Input: React.FC<Props> = ({ type, placeholder, value, onChange }) => {
+const Input: React.FC<Props> = ({ type, name, placeholder, value, onChange }) => {
     return (
         <C.Input
             value={value}
+            name={name}
             onChange={onChange}
             type={type}
             placeholder={placeholder}
