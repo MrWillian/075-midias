c41041abef40358ef099da5d2835cb9f2e9ca072
MrWillian <willian.dev10@gmail.com>
2022-09-23 11:33:01 -0300

    style: change button width and height

--- src/components/SliderButton/style.ts
+++ src/components/SliderButton/style.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
-    width: 60px;
-    height: 60px;
+    width: 30px;
+    height: 30px;
     border-radius: 50%;
     background: rgba(241, 241, 241, 0.5);
     border: 1px solid rgba(34, 34, 34, 0.287);
