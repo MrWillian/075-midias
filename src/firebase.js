e7f42edc2d5213ea9f1e983e8e76127b4d95c218
MrWillian <willian.dev10@gmail.com>
2022-09-26 15:16:33 -0300

    docs: increase database firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,13 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 import { authConfig } from "./auth/config";
 
 const app = getApps().length > 0 ? getApps()[0] : initializeApp(authConfig);
 const auth = getAuth(app);
 export const storage = getStorage(app);
+export const database = getFirestore(app);
 
 export const logInWithEmailAndPassword = async (email, password) => {
     try {
