50da2503df7c26c546f55b9b5fa86f20d1684059
MrWillian <willian.dev10@gmail.com>
2022-09-27 14:49:39 -0300

    chore: adapt to receive icon

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { IconType } from "react-icons/lib";
 import * as C from "./styles";
 
 type Props = {
     text?: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement>,
     type?: "button" | "submit" | "reset",
-    style?: React.CSSProperties
+    style?: React.CSSProperties,
+    Icon?: IconType
 }
 
-const Button: React.FC<Props> = ({ text, onClick, type = "button", style }) => {
+const Button: React.FC<Props> = ({ text, onClick, type = "button", style, Icon }) => {
     return (
         <C.Button type={type} onClick={onClick} style={style}>
-            {text}
+            {Icon ? <Icon /> : text}
         </C.Button>
     );
 }
