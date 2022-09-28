4c9893f05e3bd60e4d32264f6235442141fefa59
MrWillian <willian.dev10@gmail.com>
2022-09-28 15:15:32 -0300

    chore: remove unused variable

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import * as C from './style';
 import { MdEast, MdWest } from 'react-icons/md';
 import SliderButton from '../SliderButton';
