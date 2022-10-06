a9b84e794109794d6abf52c6323a80c383af5f1a
MrWillian <willian.dev10@gmail.com>
2022-10-06 16:24:14 -0300

    refactor: export function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,11 @@
 import getDownloadURLForImageStorage from './storage/getDownloadURLForImageStorage';
 import getAlbumFromStorageById from './storage/getAlbumFromStorageById';
+import getPhotosFromAlbumByNameAlbum from './storage/getPhotosFromAlbumByNameAlbum';
 import sanitizeFilesToImageFile from './sanitizeFilesToImageFile';
 
-export { getDownloadURLForImageStorage, sanitizeFilesToImageFile, getAlbumFromStorageById };
+export { 
+    getDownloadURLForImageStorage, 
+    sanitizeFilesToImageFile, 
+    getAlbumFromStorageById, 
+    getPhotosFromAlbumByNameAlbum
+};
