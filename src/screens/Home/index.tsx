c40171d431dac7d70103152c077925e5a8c32794
MrWillian <willian.dev10@gmail.com>
2022-10-28 10:34:17 -0300

    refactor: select photos from cloud storage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,75 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { getDownloadURL, listAll, ref } from 'firebase/storage';
 import PhotoAlbum from 'react-photo-album';
 import ContactSection from '../../components/ContactSection';
 import EventsSection from '../../components/EventsSection';
 import Navbar from '../../components/Navbar';
+import { database, storage } from '../../firebase';
 import * as C from './style';
 
+type Photos = {
+    src: string;
+    width: number;
+    height: number;
+}
+
 const Home = () => {
+    const [photos, setPhotos] = useState<Photos[]>([]);
+    const [photosToShow, setPhotosToShow] = useState<Photos[]>([]);
 
-    const photos = [
-        {
-            src: "https://picsum.photos/id/1011/250/150/",
-            width: 800,
-            height: 600
-        },
-        {
-            src: "https://picsum.photos/id/1012/250/150/",
-            width: 1600,
-            height: 900
-        },
-        {
-            src: "https://picsum.photos/id/1013/250/150/",
-            width: 2000,
-            height: 1800
-        },
-        {
-            src: "https://picsum.photos/id/1014/250/150/",
-            width: 1800,
-            height: 1000
-        },
-        {
-            src: "https://picsum.photos/id/1015/250/150/",
-            width: 1700,
-            height: 1500
-        },
-        {
-            src: "https://picsum.photos/id/1016/250/150/",
-            width: 1600,
-            height: 1100
-        },
-        {
-            src: "https://picsum.photos/id/1020/250/150/",
-            width: 1000,
-            height: 500
-        },
-        {
-            src: "https://picsum.photos/id/1018/250/150/",
-            width: 2000,
-            height: 2000
-        },
-        {
-            src: "https://picsum.photos/id/1019/250/150/",
-            width: 2000,
-            height: 1000
-        }
-    ];
+    useEffect(() => {
+        getAlbunsFromFirestore();
+    }, []);
 
-    // const listItem = () => {
-    //     storage.ref().child('images/').listAll().then(res => {
-    //         res.items.forEach((item) => {
-    //             setData(arr => [...arr, item.name]);
-    //         })
-    //     }).catch(err => {
-    //         alert(err.message);
-    //     });
-    // }
-    
+    useEffect(() => {
+        selectPhotos();
+    }, [photos]);
+
+    const getAlbunsFromFirestore = () => {
+        let albunsName: string[] = [];
+        getDocs(collection(database, "albuns")).then((results) => {
+            results.forEach(async (doc) => {
+                albunsName.push(doc.data().name);
+                await getPhotos(doc.data().name);
+            });
+        });
+    }
+
+    const getPhotos = async (albumName: string) => {
+        listAll(ref(storage, `albuns/${albumName}`)).then((results) => {
+            results.items.forEach((item) => getDownloadURL(item).then((url) => 
+                setPhotos((prev) => [...prev, { width: 100, height: 100, src: url }])
+            ));
+        }).catch(error => console.log(error));
+    }
+
+    const selectPhotos = () => setPhotosToShow(photos.slice(0, 10));
+
     return (
         <>
             <Navbar />
             <C.Container>
-                <PhotoAlbum layout="masonry" photos={photos} />
+                <PhotoAlbum layout="columns" photos={photosToShow} />
             </C.Container>
             <EventsSection />
             <ContactSection />
