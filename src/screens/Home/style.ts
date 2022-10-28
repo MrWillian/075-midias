f6615d2f3eb9fce9dd1c4656c2c838a9b17d7f11
MrWillian <willian.dev10@gmail.com>
2022-10-28 10:54:32 -0300

    style: implement  image hover effects

--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -12,3 +12,27 @@ export const Container = styled.div`
 `;
 
 export const Title = styled.h2``;
+
+export const Row = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    margin: 5px;
+`;
+
+export const FigureImage = styled.figure`
+    overflow: hidden;
+`;
+
+export const Image = styled.img`
+    -webkit-transform: scale(1);
+	transform: scale(1);
+	-webkit-transition: .3s ease-in-out;
+	transition: .3s ease-in-out;
+
+    &:hover {
+        -webkit-transform: scale(1.3);
+	    transform: scale(1.3);
+    }
+`;
