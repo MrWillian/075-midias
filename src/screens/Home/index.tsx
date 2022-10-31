d2d01e7cc65d72c35a772bda84a69f59c5132f91
MrWillian <willian.dev10@gmail.com>
2022-10-31 14:49:52 -0300

    refactor: implement photos shuffled array on home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -12,6 +12,7 @@ import { sleep } from '../../utils';
 import { useNavigate } from "react-router-dom";
 import { Button } from '../../components';
 import { MdRemoveRedEye } from "react-icons/md";
+import shuffle from '../../utils/shuffle';
 
 type Photos = {
     id: string;
@@ -28,7 +29,7 @@ const Home = () => {
     useEffect(() => {
         const loadingContent = async () => {
             getAlbunsFromFirestore();
-            await sleep(2000);
+            await sleep(3000);
         }
         trackPromise(loadingContent().catch(error => console.log(error)));
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -57,7 +58,7 @@ const Home = () => {
         }).catch(error => console.log(error));
     }
 
-    const selectPhotos = () => setPhotosToShow(photos.slice(0, 10));
+    const selectPhotos = () => setPhotosToShow(shuffle(photos).slice(0, 10));
 
     const showAlbum = (id: string) => navigate(`/show-album/${id}`);
 
