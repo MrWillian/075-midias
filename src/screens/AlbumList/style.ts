cd34f3ed1c14cedca06c5d8cae2e66af9010f480
MrWillian <willian.dev10@gmail.com>
2022-11-04 15:45:49 -0300

    style: create styled components to screen

---
+++ src/screens/AlbumList/style.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center; 
+    justify-content: center; 
+    flex-direction: column; 
+    height: 100%; 
+    width: 100%; 
+    padding: 30px 0;
+`;
+
+
+export const List = styled.div`
+    display: flex;
+    align-items: center; 
+    justify-content: center; 
+    flex-direction: column; 
+    background-color: #CCC;
+    border-radius: 15px;
+    padding: 10px 0;
+    height: 50%; 
+    width: 50%; 
+    webkit-box-shadow: 10px 10px 15px 0px rgba(0,0,0,0.75);
+    moz-box-shadow: 10px 10px 15px 0px rgba(0,0,0,0.75);
+    box-shadow: 10px 10px 15px 0px rgba(0,0,0,0.75);
+`;
