1232380274fb19b015800a38e97f1558a6f2557e
MrWillian <willian.dev10@gmail.com>
2022-11-04 15:21:36 -0300

    style: define media query to responsiveness on events

--- src/components/EventsSection/style.ts
+++ src/components/EventsSection/style.ts
@@ -7,9 +7,8 @@ export const Container = styled.div`
     height: 100vh;
     gap: 20px;
 
-    @media screen and (max-width: 1000px) {
+    @media screen and (max-width: 900px) {
         flex-direction: column;
-        margin: 20px 0;
     }
 `;
 
@@ -19,6 +18,15 @@ export const TextContainer = styled.div`
     justify-content: center;
     flex-direction: column;
     width: 70vh;
+
+    @media screen and (max-width: 700px) {
+        width: auto;
+    }
+
+    @media screen and (max-width: 600px) {
+        padding: 25px;
+    }
+
 `;
 
 export const ImageContainer = styled.div`
@@ -27,10 +35,39 @@ export const ImageContainer = styled.div`
     justify-content: center;
     width: 70vh;
     height: 50vh;
+
+    @media screen and (max-width: 700px) {
+        width: 60vh;
+        height: 40vh;   
+    }
+
+    @media screen and (max-width: 600px) {
+        width: 55vh;
+        height: 35vh;
+    }
+
+    @media screen and (max-width: 500px) {
+        width: 45vh;
+        height: 30vh;
+    }
+    
+    @media screen and (max-width: 400px) {
+        width: 40vh;
+        height: 20vh;   
+    }
+
 `;
 
 export const Title = styled.h2`
     text-transform: uppercase;
+
+    @media screen and (max-width: 700px) {
+        font-size: 1.5rem;
+    }
+
+    @media screen and (max-width: 900px) and (max-height: 600px) {
+        font-size: 1.2rem;
+    }
 `;
 
 export const Line = styled.hr`
@@ -43,6 +80,14 @@ export const Line = styled.hr`
 export const Content = styled.text`
     font-size: 2rem;
     margin: 0.5rem 0;
+
+    @media screen and (max-width: 700px) {
+        font-size: 1rem;
+    }
+
+    @media screen and (max-width: 900px) and (max-height: 600px) {
+        font-size: 1rem;
+    }
 `;
 
 export const ContactLink = styled.a`
