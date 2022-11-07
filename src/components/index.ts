f217dfc8071798d64b96bca0ef7dfc6011248281
MrWillian <willian.dev10@gmail.com>
2022-11-07 09:28:23 -0300

    chore: export Slider from index component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import Input from './Input';
 import Button from './Button';
 import ContactSection from './ContactSection';
 import EventsSection from './EventsSection';
+import Slider from './Slider';
 
-export { Button, Input, Navbar, ContactSection, EventsSection };
+export { Button, Input, Navbar, ContactSection, EventsSection, Slider };
